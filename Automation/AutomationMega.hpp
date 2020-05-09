e19707ce15fd6f1965f5ba7ce40b3b270f1e1bb6
sztanderap <sztandera.p@gmail.com>
2020-05-09 16:15:01 +0100

    change automation logic

--- Automation/AutomationMega.hpp
+++ Automation/AutomationMega.hpp
@@ -12,25 +12,29 @@
 
 #include "../Mapping/MappingMega.hpp"
 
+void myDelay(long interval) {
+    unsigned long currentMillis = millis();
+    while(millis() - currentMillis < interval){}
+}
+
 // relay acts as a click button
-void relayAsPushButton(int pin, bool activeLow, long interval) {
+void relayAsPushButton(int pin, bool activeLow) {
   uint8_t state = digitalRead(pin);
   unsigned long currentMillis = millis();
-    
   digitalWrite(pin, !state);
-  while(millis() - currentMillis < interval){}
+  myDelay(125);
   digitalWrite(pin, state);
 }
 
 // relay acts as a latch button
 void relayAsLatchButton(int pin, bool activeLow) {
-  uint8_t state = digitalRead(pin);  
-  digitalWrite(pin, !state);
+  uint8_t pinState = digitalRead(pin);
+  digitalWrite(pin, !pinState);
 }
 
 
 
-void setOutput(const uint8_t& sensorId, const uint8_t& cmd = Toggle::FLIP, bool isLatch = false) {
+void setOutput(const uint8_t& sensorId, const uint8_t& cmd = Toggle::FLIP, bool actAsPushButton = true) {
 
 // Sensors[index]
   uint8_t index = getIndex(sensorId);
@@ -38,58 +42,49 @@ void setOutput(const uint8_t& sensorId, const uint8_t& cmd = Toggle::FLIP, bool 
 
   // check whether flip state of sensor
   uint8_t state = (cmd == Toggle::FLIP) ? !loadState(sensor.id) : cmd;
-  
-    if(cmd == Toggle::FLIP && isLatch) {
+//  saveState(sensor.id, state);
+
+    if(cmd == Toggle::FLIP && actAsPushButton) {
         
-        relayAsPushButton(sensor.pin, sensor.activelow, 100);
-        
-    } else if (cmd == Toggle::FLIP && isLatch) {
-        
+        relayAsPushButton(sensor.pin, sensor.activelow);
+        send(msgs[index].set(!state));
+    } else if (cmd == Toggle::FLIP && !actAsPushButton) {
         relayAsLatchButton(sensor.pin, sensor.activelow);
         send(msgs[index].set(state));
-        
+
     } else {
-        
+        saveState(sensor.id, state);
         send(msgs[index].set(state));
     }
-
+//    const uint8_t buttonsSize = sizeof(getButtons(sensor.id)) / sizeof(OneButton);
+//    for (uint8_t i = 0; i < buttonsSize; i++) {
+//        OneButton buttons [buttonsSize] = getButtons(sensor.id);
+//        if(!buttons[i].isLongPressed()) {
+//            state = !loadState(sensor.id);
+//            saveState(sensor.id, state);
+//            send(msgs[index].set(state));
+//        }
+//    }
 }
-
 
 void setStateON(void* pSensorId) {
   auto sensor = getSensor(pSensorId);
-  setOutput(sensor.id, Toggle::ON);
+    setOutput(sensor.id, Toggle::ON);
 }
 
 void setStateOFF(void* pSensorId) {
   auto sensor = getSensor(pSensorId);
-  setOutput(sensor.id, Toggle::OFF);
+    setOutput(sensor.id, Toggle::OFF);
 }
 
-
 void flipSwitch(void* pSensorId) {
-    
-  
   auto sensor = getSensor(pSensorId);
   setOutput(sensor.id);
-    
-  uint8_t index = getIndex(pSensorId);
-  const uint8_t buttonsSize = sizeof(getButtons(sensor.id)) / sizeof(OneButton);
-    
-  for (uint8_t i = 0; i < buttonsSize; i++) {
-            
-    OneButton buttons [buttonsSize] = getButtons(sensor.id);
-      if(buttons[i].isLongPressed()) {
-          return;
-      }
-  }
-  saveState(sensor.id, !loadState(sensor.id));
-  send(msgs[index].set(!loadState(sensor.id)));
 }
 
 void latchSwitch(void* pSensorId) {
   auto sensor = getSensor(pSensorId);
-  setOutput(sensor.id, Toggle::FLIP, true);
+  setOutput(sensor.id, Toggle::FLIP, false);
 }
 
 void setupButtons() {
